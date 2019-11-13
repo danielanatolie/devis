cbca3c29fa1fc0766dbb3a40ffaa5ee95da71e75
Ari Khuyagbaatar <thearikh@gmail.com>
2019-11-12 16:40:03 -0800

    minor css change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import './App.css';
-import { ProgressBar, ButtonToolbar, Button, Table} from 'react-bootstrap';
+import { ProgressBar, ButtonToolbar, Button, Table } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export class App extends Component {
@@ -13,22 +13,32 @@ export class App extends Component {
     const response = await fetch('/api/test');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
-    console.log("server response = ", body);
+    console.log('server response = ', body);
   };
 
   render() {
     return (
-    <div>
-      <h>Overall Code Rating</h>
-      <ProgressBar now={60} />
-      <br></br>
-      <ButtonToolbar>
-        <Button onClick={this.handleClick} variant="primary">Analyze Complexity</Button>
-      </ButtonToolbar>
-      <br></br>
-    </div>
-  );
-    }
+      <div className="App">
+        <header>
+          <p className="title">Devis</p>
+        </header>
+
+        <body>
+          <div className="container">
+            <h>Overall Code Rating</h>
+            <ProgressBar now={60} />
+            <br></br>
+            <ButtonToolbar>
+              <Button onClick={this.handleClick} variant="primary">
+                Analyze Complexity
+              </Button>
+            </ButtonToolbar>
+            <br></br>
+          </div>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;

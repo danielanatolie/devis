cbb49dd40c741ad787b34ab62a62adf5c4b4c272
Ari Khuyagbaatar <thearikh@gmail.com>
2019-11-13 14:08:39 -0800

    add animation and horizontal line

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,14 +1,16 @@
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
 const data = [
-  { name: 'file 1', debt: 25 },
-  { name: 'file 2', debt: 15 },
-  { name: 'file 3', debt: 30 },
-  { name: 'file 4', debt: 20 }
+  { name: "file 1", debt: 25 },
+  { name: "file 2", debt: 15 },
+  { name: "file 3", debt: 30 },
+  { name: "file 4", debt: 20 }
 ];
+
 const MARGIN = { TOP: 10, BOTTOM: 50, LEFT: 70, RIGHT: 10 };
 const WIDTH = 500 - MARGIN.LEFT - MARGIN.RIGHT;
 const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
+const DANGER_ZONE = 100;
 export default class D3Chart {
   constructor(element) {
     const max = d3.max(data, d => {
@@ -21,11 +23,11 @@ export default class D3Chart {
 
     const svg = d3
       .select(element)
-      .append('svg')
-      .attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-      .attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
-      .append('g')
-      .attr('transform', `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
+      .append("svg")
+      .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+      .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+      .append("g")
+      .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
     const y = d3
       .scaleLinear()
@@ -41,48 +43,61 @@ export default class D3Chart {
     const xAxis = d3.axisBottom(x);
     const yAxis = d3.axisLeft(y);
     svg
-      .append('g')
-      .attr('transform', `translate(0, ${HEIGHT})`)
-      .attr('stroke', 'white')
+      .append("g")
+      .attr("transform", `translate(0, ${HEIGHT})`)
+      .attr("stroke", "white")
       .call(xAxis);
     svg
-      .append('g')
-      .attr('stroke', 'white')
+      .append("g")
+      .attr("stroke", "white")
       .call(yAxis);
 
-    svg.selectAll('path').style('stroke', 'white');
-    svg.selectAll('line').style('stroke', 'white');
+    svg.selectAll("path").style("stroke", "white");
+    svg.selectAll("line").style("stroke", "white");
     svg
-      .append('text')
-      .attr('x', WIDTH / 2)
-      .attr('y', HEIGHT + 40)
-      .attr('text-anchor', 'middle')
-      .attr('stroke', 'white')
-      .text('File Name');
+      .append("text")
+      .attr("x", WIDTH / 2)
+      .attr("y", HEIGHT + 40)
+      .attr("text-anchor", "middle")
+      .attr("stroke", "white")
+      .text("File Name");
 
     svg
-      .append('text')
-      .attr('x', -(HEIGHT / 2))
-      .attr('y', -50)
-      .attr('text-anchor', 'middle')
-      .attr('stroke', 'white')
-      .text('Debt')
-      .attr('transform', 'rotate(-90)');
+      .append("text")
+      .attr("x", -(HEIGHT / 2))
+      .attr("y", -50)
+      .attr("text-anchor", "middle")
+      .attr("stroke", "white")
+      .text("Debt")
+      .attr("transform", "rotate(-90)");
 
-    const rects = svg.selectAll('rect').data(data);
+    const rects = svg.selectAll("rect").data(data);
+
+    // adding danger zone
+    svg
+      .append("line")
+      .attr("stroke", "white")
+      .attr("stroke-dasharray", "5,5")
+      .attr("y1", DANGER_ZONE)
+      .attr("x1", 0)
+      .attr("y2", DANGER_ZONE)
+      .attr("x2", WIDTH);
 
     rects
       .enter()
-      .append('rect')
-      .attr('x', d => x(d.name))
-      .attr('y', d => y(d.debt))
-      .attr('width', 50)
-      .attr('height', d => HEIGHT - y(d.debt))
-      .attr('fill', d => {
+      .append("rect")
+      .attr("x", d => x(d.name))
+      .attr("width", 50)
+      .attr("fill", d => {
         if (d.debt > 20) {
-          return 'red';
+          return "red";
         }
-        return 'grey';
-      });
+        return "grey";
+      })
+      .attr("y", HEIGHT)
+      .transition()
+      .duration(700)
+      .attr("y", d => y(d.debt))
+      .attr("height", d => HEIGHT - y(d.debt));
   }
 }

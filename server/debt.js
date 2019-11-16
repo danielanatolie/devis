570d358a9626ff4c43dc6464b878b24802a6e01a
Daniel Anatolie <daniel.anatolie@gmail.com>
2019-11-16 12:58:27 -0800

    build fileToDebtMap

--- server/debt.js
+++ server/debt.js
@@ -1,23 +1,87 @@
 const fs = require('fs');
+const testFolderPath = './test/project1/files';
+const _ = require('underscore');
 
-processDirectory = async () => {
-    return await computeComplexityScore(extractFileContents());
-}
+let fileToDebtMap = new Map();
 
-extractFileContents = async () => {
-    return fs.readdir(path, function(err, items) {
-        return fileContents = items.map((fileName) => {
-            return await fs.readFileSync(__dir + "/" + fileName, 'utf8');
-        })
+buildFileToDebtMap = async () => {
+    return fs.readdir(testFolderPath, function(err, items) {
+        items.map((fileName) => {
+            getFileDebt(fileName);
+        });
     });
 }
 
-computeComplexityScore = (fileContents) => {
-    let fileToDebt;
-    /*  TODO
-        1. split on file lines
-        2. if line = funcDec, begin parameter analysis, spawn line, and tab spacing counter
-        3. compute & return Map
-    */
-    return fileToDebt;
+getFileDebt = async (fileName) => {
+    fs.readFile(testFolderPath+"/"+fileName, {encoding: 'utf-8'}, function(err,data){
+        analyzeFile(fileName, data.split("\n"));
+    });
 }
+
+analyzeFile = (fileName, fileDataArr) => {
+    let fnCount = 0;
+    let fnToDataMap = new Map();
+    console.log("analyzing file: ", fileName);
+    let lineNum = 0;
+    while (lineNum < fileDataArr.length) {
+        if (isFunctionDec(fileDataArr[lineNum++])) {
+            let braceBalance = 1;
+            let fnData = [];
+            while (braceBalance != 0) {
+                if (_.isUndefined(fileDataArr[lineNum])) break;
+                braceBalance += computeBraceBalance(fileDataArr[lineNum]);
+                fnData.push(fileDataArr[lineNum++]);
+            }
+            fnToDataMap.set(fnCount, fnData);
+            fnCount++;
+        }
+    }
+    computeFileComplexity(fileName, fnToDataMap);
+}
+
+isFunctionDec = (fileLine) => {
+    return fileLine.includes("):") && fileLine.includes("{");
+}
+
+computeBraceBalance = (fileLine) => {
+    if (fileLine.includes("{") && fileLine.includes("}")) return 0;
+    else if (fileLine.includes("{")) return 1;
+    else if (fileLine.includes("}")) return -1;
+    else return 0;
+}
+
+computeFileComplexity = (fileName, fnToDataMap) => {
+    let totalFileDebt = 0;
+    for (let i = 0; i < fnToDataMap.size; i++) {
+        totalFileDebt += computeFnDebt(fnToDataMap.get(i));
+    }
+    console.log(fileName, " has debt = ", totalFileDebt);
+    fileToDebtMap.set(fileName, totalFileDebt);
+}
+
+computeFnDebt = (fnData) => {
+    let fnDebt = 0;
+    if (isLongFunction(fnData)) fnDebt++;
+    if (hasLongLines(fnData)) fnDebt++;
+    if (hasManyIfs(fnData)) fnDebt++;
+    return fnDebt;
+}
+
+isLongFunction = (fnData) => { return fnData.length > 10; }
+
+hasLongLines = (fnData) => {
+    let totalLineLen = 0;
+    for (let i = 0; i < fnData.length; i++) {
+        totalLineLen += fnData[i].length;
+    }
+    return totalLineLen/fnData.length > 40;
+}
+
+hasManyIfs = (fnData) => {
+    let ifCount = 0;
+    for (let i = 0; i < fnData.length; i++) 
+        if (fnData[i].includes("if")) ifCount++;
+    return ifCount >= 3;
+}
+
+buildFileToDebtMap();

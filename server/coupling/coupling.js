b7d1d9b20f79b90d898fa2c7eae0de206a92e2e9
Jasmin <jasmin.liyi97@gmail.com>
2019-11-16 16:49:18 -0800

    Jasmin - Extract coupling data (#9)

---
+++ server/coupling/coupling.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const FileAnalyzer = require('./fileAnalyzer');
+
+class Coupling {
+    constructor(projectName) {
+        this.projectName = projectName;
+        this.tsFiles = [];
+    }
+
+    getTSFiles() {
+        fs.readdirSync("test/project1/files").forEach(filename => {
+            if (filename.split(".").pop() == "ts") {
+                this.tsFiles.push(filename);
+            }
+        });
+    }
+
+    countAllDependencies() {
+        let result = {};
+        this.getTSFiles()
+        this.tsFiles.forEach((filename) => {
+            const fileAnalyzer = new FileAnalyzer(this.projectName, filename);
+            const fileDependencies = fileAnalyzer.countDependencies();
+            for (let dependency in fileDependencies) {
+                result[dependency] = fileDependencies[dependency];
+            }
+        })
+        return result;
+
+    }
+}
+
+module.exports = Coupling;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+const Coupling = require('./coupling/coupling');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
 app.get('/api/test', (req, res) => {
-  const name = req.query.name || 'World';
+  // const name = req.query.name || 'World';
+  const projectName = req.query.name || "project1";
+  const couplingData = new Coupling(projectName).countAllDependencies();
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+  // res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+  res.send(JSON.stringify(couplingData));
 });
 
 app.listen(3001, () =>

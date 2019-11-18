d8be2ca40973f224e9ffc10d07cc3a53bb463284
Daniel Anatolie <daniel.anatolie@gmail.com>
2019-11-17 16:29:05 -0800

    Daniel - Extract File Debt Data (#11)
    * Partial debt parser implementation
    
    * build fileToDebtMap
    
    * Handle race conditions
    
    * Improve code quality
    
    * filter out non .ts files

--- server/index.js
+++ server/index.js
@@ -4,16 +4,16 @@ const pino = require('express-pino-logger')();
 const Coupling = require('./coupling/coupling');
 
 const app = express();
+const fileDebt = require('./debt.js')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
-app.get('/api/test', (req, res) => {
-  // const name = req.query.name || 'World';
+app.get('/api/test', async (req, res) => {
   const projectName = req.query.name || "project1";
   const couplingData = new Coupling(projectName).countAllDependencies();
+  const fileToDebtMap = await fileDebt.buildFileToDebtMap();
   res.setHeader('Content-Type', 'application/json');
-  // res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-  res.send(JSON.stringify(couplingData));
+  res.send(JSON.stringify({ fileToDebtMap, couplingData }));
 });
 
 app.listen(3001, () =>
